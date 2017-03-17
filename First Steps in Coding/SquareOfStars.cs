8606f2bd9347e09627a8739aecb7cd1529e75561
elitsaleontieva <ellyleontieva@abv.bg>
2017-03-18 00:48:23 +0200

    Add files via upload

---
+++ First Steps in Coding/SquareOfStars.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareOfStars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string spaces = new string(' ', n - 2);
+            string startingStars = new string('*', n);
+
+            Console.WriteLine(startingStars);
+
+            for (int i = 1; i <= (n - 2); i++)
+            {
+                Console.WriteLine("*" + spaces + "*");
+            }
+            Console.WriteLine(startingStars);
+        }
+    }
+}
